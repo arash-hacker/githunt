6a224c3907cf20cdbe1e166ede0f1b5881d2647d
Arash Aghajani <arash.aghajani@morressier.com>
2024-03-27 13:47:49 +0100

    Updates

---
+++ precache-manifest.5b01b2734041c5e5e10d79306b4c58cd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01b093d5ebb658e431368f37f8c283e9",
+    "url": "/githunt/index.html"
+  },
+  {
+    "revision": "6d8354432230d29c0dc5",
+    "url": "/githunt/static/css/2.92ac36f0.chunk.css"
+  },
+  {
+    "revision": "98867b41f4c94a1d5270",
+    "url": "/githunt/static/css/main.5b277643.chunk.css"
+  },
+  {
+    "revision": "6d8354432230d29c0dc5",
+    "url": "/githunt/static/js/2.af73dbb9.chunk.js"
+  },
+  {
+    "revision": "b22fd9b4a3d7ad3f113bfdae2fd33eec",
+    "url": "/githunt/static/js/2.af73dbb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98867b41f4c94a1d5270",
+    "url": "/githunt/static/js/main.d88f6a38.chunk.js"
+  },
+  {
+    "revision": "42e6b907cc11bbc258a6",
+    "url": "/githunt/static/js/runtime-main.fe817508.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/githunt/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/githunt/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/githunt/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/githunt/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/githunt/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
